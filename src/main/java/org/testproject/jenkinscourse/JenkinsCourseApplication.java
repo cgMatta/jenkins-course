f0639f86bb29984eef2d2e192199383d67d91f93
cemartinez <cemartinez@griddynamics.com>
2024-10-01 21:04:37 -0600

    Init Commit

---
+++ src/main/java/org/testproject/jenkinscourse/JenkinsCourseApplication.java
@@ -0,0 +1,13 @@
+package org.testproject.jenkinscourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsCourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/testproject/jenkinscourse/JenkinsCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.testproject.jenkinscourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
